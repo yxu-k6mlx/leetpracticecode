048497ac3082c1660a07b9e74a859d3860f2b6f5
myland123456 <xuyanwen2xz@gmail.com>
2024-09-28 13:59:36 -0700

    init LongestPalindromicSubstring

---
+++ LongestPalindromicSubstring.py
@@ -0,0 +1,22 @@
+class Solution:
+    def __init__(self, input: str): 
+        print(self.longestPalindrome(input))
+        return 
+    
+    def longestPalindrome(self, s: str) -> str:
+        pass 
+    
+    def left(self): 
+        pass 
+
+    def right(self): 
+        pass 
+
+if __name__ == '__main__': 
+    testcases = [
+        'a', 
+        'b'
+    ]
+    for case in testcases: 
+        sol = Solution(case)
+    
